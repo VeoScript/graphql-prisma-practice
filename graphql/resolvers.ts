7cc38d1f15a5bc49ddfcb749a38a8ae0bbf9d4ad
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-03-12 20:15:54 +0800

    refactor: update graphql resolvers _args

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -7,35 +7,35 @@ export const resolvers = {
         id: 'desc'
       }]
     }),
-    getStudent: async (_parent: any, _args: any, ctx: any) => await ctx.prisma.student.findUnique({
+    getStudent: async (_parent: any, { id }: any, ctx: any) => await ctx.prisma.student.findUnique({
       where: {
-        id: _args.id
+        id: id
       }
     })
   },
   Mutation: {
-    addStudent: async (_parent: any, _args: any, ctx: any) => await ctx.prisma.student.create({
+    addStudent: async (_parent: any, { name, age, gender, course }: any, ctx: any) => await ctx.prisma.student.create({
       data: {
-        name: _args.name,
-        age: _args.age,
-        gender: _args.gender,
-        course: _args.course
+        name,
+        age,
+        gender,
+        course
       }
     }),
-    updateStudent: async (_parent: any, _args: any, ctx: any) => await ctx.prisma.student.update({
+    updateStudent: async (_parent: any, { id, name, age, gender, course }: any, ctx: any) => await ctx.prisma.student.update({
       where: {
-        id: _args.id
+        id: id
       },
       data: {
-        name: _args.name,
-        age: _args.age,
-        gender: _args.gender,
-        course: _args.course
+        name,
+        age,
+        gender,
+        course
       }
     }),
-    deleteStudent: async (_parent: any, _args: any, ctx: any) => await ctx.prisma.student.delete({
+    deleteStudent: async (_parent: any, { id }: any, ctx: any) => await ctx.prisma.student.delete({
       where: {
-        id: _args.id
+        id: id
       }
     })
   }
