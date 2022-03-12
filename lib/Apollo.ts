79d7f21ce56e49246883f3991c337f533bcf709f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-03-12 21:24:47 +0800

    wip: testing apollo client instance

--- lib/Apollo.ts
+++ lib/Apollo.ts
@@ -1,16 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { relayStylePagination } from '@apollo/client/utilities'
 
-let dynamic_uri
-
-if (process.env.NODE_ENV === 'production') {
-  dynamic_uri = process.env.PRODUCTION_URI
-} else {
-  dynamic_uri = 'http://localhost:3000/api/graphql'
-}
-
 export const client = new ApolloClient({
-  uri: dynamic_uri,
+  uri: 'http://localhost:3000/api/graphql',
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
