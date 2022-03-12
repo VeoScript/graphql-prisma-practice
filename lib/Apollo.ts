d9c802174d7cd7f6ce7515f3f342ea9d816770f8
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-03-13 00:11:25 +0800

    chore: update dynamic uri for production and development

--- lib/Apollo.ts
+++ lib/Apollo.ts
@@ -1,8 +1,16 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { relayStylePagination } from '@apollo/client/utilities'
 
+let dynamic_uri
+
+if (process.env.NODE_ENV === 'production') {
+  dynamic_uri = 'http://localhost:3000/api/graphql'
+} else {
+  dynamic_uri = process.env.PRODUCTION_URI
+}
+
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: dynamic_uri,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    PRODUCTION_URI: process.env.PRODUCTION_URI
+  }
 }
 
 module.exports = nextConfig
