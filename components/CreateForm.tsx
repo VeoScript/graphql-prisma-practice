a2f68561eee0a49c7ae2f6d9c2772d0124ca971d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-03-13 12:19:03 +0800

    feat: add toaster (#12)

--- components/CreateForm.tsx
+++ components/CreateForm.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Router from 'next/router'
+import { toast } from 'react-hot-toast'
 import { useForm } from 'react-hook-form'
 import { useMutation } from '@apollo/client'
 import { CREATE_STUDENT_MUTATION } from '../graphql/modules/mutations'
 import DialogLayout from '../layouts/dialog'
-import { RiAddLine, RiCloseLine } from 'react-icons/ri'
+import { RiAddLine, RiCheckboxCircleFill, RiCloseLine } from 'react-icons/ri'
 import Spinner from '../utils/Spinner'
 
 interface FormData {
@@ -40,6 +41,38 @@ const CreateForm = () => {
     const variables = { name, age, gender, course }
 
     await addStudent({ variables })
+
+    toast.custom((t) => (
+      <div
+        className={`${
+          t.visible ? 'animate-enter' : 'animate-leave'
+        } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+      >
+        <div className="flex-1 w-0 p-4">
+          <div className="flex items-start">
+            <div className="flex-shrink-0 pt-0.5">
+              <RiCheckboxCircleFill className="w-10 h-10 text-blue-500" />
+            </div>
+            <div className="ml-3 flex-1">
+              <p className="text-sm font-medium text-gray-900">
+                { name }
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                Student successfully added.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex border-l border-gray-200">
+          <button
+            onClick={() => toast.dismiss(t.id)}
+            className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    ))
 
     reset()
     closeModal()
--- components/EditForm.tsx
+++ components/EditForm.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Router from 'next/router'
+import { toast } from 'react-hot-toast'
 import { useForm } from 'react-hook-form'
 import { useMutation } from '@apollo/client'
 import { UPDATE_STUDENT_MUTATION } from '../graphql/modules/mutations'
 import DialogLayout from '../layouts/dialog'
-import { RiEditBoxLine, RiCloseLine } from 'react-icons/ri'
+import { RiEditBoxLine, RiCloseLine, RiRefreshFill } from 'react-icons/ri'
 import Spinner from '../utils/Spinner'
 
 interface IProps {
@@ -52,6 +53,38 @@ const EditForm: React.FC<IProps> = ({ student }) => {
     const variables = { getId, name, age, gender, course }
 
     await editStudent({ variables })
+
+    toast.custom((t) => (
+      <div
+        className={`${
+          t.visible ? 'animate-enter' : 'animate-leave'
+        } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+      >
+        <div className="flex-1 w-0 p-4">
+          <div className="flex items-start">
+            <div className="flex-shrink-0 pt-0.5">
+              <RiRefreshFill className="w-10 h-10 text-green-500" />
+            </div>
+            <div className="ml-3 flex-1">
+              <p className="text-sm font-medium text-gray-900">
+                { name }
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                Student updated successfully.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex border-l border-gray-200">
+          <button
+            onClick={() => toast.dismiss(t.id)}
+            className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    ))
 
     reset()
     closeModal()
